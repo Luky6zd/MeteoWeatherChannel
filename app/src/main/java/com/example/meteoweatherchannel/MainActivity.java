f0267fa277a4b5568196b72b6cbfa9b82718c8cc
Lucrezia Odrljin <lucrezia.mestrov@gmail.com>
2025-01-16 17:46:28 +0100

    Created local variables and linked them with layout by its ID (defined in View)

--- app/src/main/java/com/example/meteoweatherchannel/MainActivity.java
+++ app/src/main/java/com/example/meteoweatherchannel/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.meteoweatherchannel;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // initializing variables
+    private RelativeLayout home;
+    private ProgressBar loading;
+    private TextView cityName, temperature, condition;
+    private TextInputEditText cityEdit;
+    private ImageView background, icon, searchIcon;
+    private RecyclerView weather;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +33,18 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        // linking local variables with layout by its ID (defined in View)
+        // findViewById returns a reference to the view
+        home = findViewById(R.id.home);
+        loading = findViewById(R.id.progressBarLoading);
+        cityName = findViewById(R.id.cityName);
+        temperature = findViewById(R.id.temperature);
+        condition = findViewById(R.id.condition);
+        cityEdit = findViewById(R.id.editCity);
+        background = findViewById(R.id.background);
+        icon = findViewById(R.id.icon);
+        searchIcon = findViewById(R.id.searchIcon);
+        weather = findViewById(R.id.weather);
+
     }
 }
